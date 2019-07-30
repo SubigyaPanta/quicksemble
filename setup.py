02fc85c777a5ef66f55222079331fe8298d9a262
Subigya Jyoti Panta <subigya@t2pco.com>
2019-07-30 16:52:34 +0700

    Added Setup informations

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as handle:
 
 
 setuptools.setup(
-    name='ensembler',
+    name='quicksemble',
     version='0.1',
     author='Subigya Jyoti Panta',
-    author_email='subigya@t2pco.com',
+    author_email='subigyapanta@gmail.com',
     description='A package to build ensemble for quick experiments.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="",
+    url="https://github.com/SubigyaPanta/quicksemble",
     packages=setuptools.find_packages(exclude=['tests*']),
     classifiers=[
         'Programming Language :: Python 3',
